f9ca04bc7267119dcf3f3077fc1f56ec7ae6004c
nikp29 <nikp29@users.noreply.github.com>
2018-05-24 14:21:36 -0700

    i should be done!

--- www/js/index.js
+++ www/js/index.js
@@ -24,7 +24,7 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
         document.getElementById("border").addEventListener('click', this.takePhoto);
         document.getElementById("right-navbar").addEventListener('click', this.restartCameraView);
-        this.initCameraView();
+        // this.initCameraView();
     },
     // deviceSynced: Boolean() = false,
     // 0 == 
@@ -48,12 +48,8 @@ var app = {
             frequency: 10
         }; // Update every .05 seconds
         this.startCameraAbove();
-        this.initCameraView();
-    },
-    initCameraView: function() {
         window.addEventListener("deviceorientation", this.handleOrientation, true);
         screen.orientation.lock('portrait-primary');
-        console.log("hi");
         var cameraView = document.getElementById('camera-interface');
         cameraView.style.display = "block";
         var analyzeView = document.getElementById('analyze-interface');
@@ -61,31 +57,34 @@ var app = {
         CameraPreview.show();
         document.getElementById("border").style.position = "absolute";
         document.getElementById("border").style.display = "block";
-
     },
+    // initCameraView: function() {
+        
+        
+
+    // },
     restartCameraView: function() {
 
         window.addEventListener("deviceorientation", this.handleOrientation, true);
-
         var image = document.getElementById('my-image');
+        image.src = "#";
+        var canvas = document.getElementById("canvasMan");
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
         var analyzeView = document.getElementById('analyze-interface');
         var cameraView = document.getElementById('camera-interface');
         var rightIcon = document.getElementById('right-icon')
-
+        
         cameraView.style.display = "block";
         cameraView.style.color = "rgba(0,0,0,0)";
         document.getElementById("border").style.position = "absolute";
         document.getElementById("border").style.display = "block";
         document.getElementById("main-text").innerHTML = "Calculating";
         analyzeView.style.display = "none";
-        console.log(analyzeView.style.display);
         CameraPreview.show();
-
-
 
     },
     initAnalyzeView: function() {
-        window.removeEventListener("deviceorientation", this.handleOrientation, true);
+        //window.removeEventListener("deviceorientation", this.handleOrientation, true);
         var cameraView = document.getElementById('camera-interface');
         cameraView.style.display = "none";
         var analyzeView = document.getElementById('analyze-interface');
@@ -124,7 +123,6 @@ var app = {
                 // this.deviceSynced = false;
                 border.style.borderColor = "rgba(63, 140, 233,0)";
                 if (rightIcon.classList.contains("fa-spin") == true) {
-                    console.log("true");
                 } else {
                     rightIcon.classList.add("fas");
                     rightIcon.classList.add("fa-spin");
@@ -132,14 +130,11 @@ var app = {
                     rightIcon.classList.remove("far");
                     rightIcon.classList.remove("fa-check-circle");
                 }
-                console.log(gamma / 4)
 
                 if (gamma > 0) { // Left/Right
-                    console.log("moister")
                     left.style.color = "rgba(63, 140, 233," + (gamma / 8).toString() + ")"; //Left
                     right.style.color = "rgba(63, 140, 233,0)";
                 } else {
-                    console.log("cloister");
                     right.style.color = "rgba(63, 140, 233," + (-1 * gamma / 8).toString() + ")"; //Right
                     left.style.color = "rgba(63, 140, 233,0)";
                 }
@@ -162,16 +157,6 @@ var app = {
 
 
     },
-
-    restartApp: function() {
-        var image = document.getElementById('myImage');
-        var photoButton = document.getElementById('deviceready');
-        var restartButton = document.getElementById('restart-button');
-        image.setAttribute('style', 'display:none;');
-        photoButton.setAttribute('style', 'display:block;');
-        restartButton.setAttribute('style', 'display:none;');
-        document.getElementById("main-text").innerHTML = "Take a Photo to Begin";
-    },
     startCameraAbove: function() {
         CameraPreview.startCamera({ x: 0, y: (window.screen.height * .1), width: window.screen.width, height: (window.screen.height * .9), toBack: true, previewDrag: false, tapPhoto: false });
         CameraPreview.setFlashMode(CameraPreview.FLASH_MODE.OFF);
@@ -180,14 +165,16 @@ var app = {
     stopCamera: function() {
         CameraPreview.stopCamera();
     },
-
-
+    
     takePhoto: function() {
         console.log("recognized func");
         if (document.getElementById('right-icon').classList.contains("fa-check-circle") == true) {
+            console.log("eventtriggered");
             CameraPreview.takePicture(function(base64PictureData) {
                 /* code here */
+                
                 imageSrcData = 'data:image/jpeg;base64,' + base64PictureData;
+                console.log("1")
                 cameraSuccess(imageSrcData);
             });
 
@@ -196,7 +183,7 @@ var app = {
                 // import ImageParser from 'js/image-parser.js';
                 // Display the image we just took,  replace the picture taking element with a restart 
                 // button, and give the canopy cover value
-                console.log("reached");
+                console.log("2");
 
                 var image = document.getElementById('my-image');
                 var analyzeView = document.getElementById('analyze-interface');
@@ -208,17 +195,21 @@ var app = {
                 document.getElementById("border").style.display = "none";
                 document.getElementById("main-text").innerHTML = "Calculating";
                 analyzeView.style.display = "block";
-                console.log(analyzeView.style.display);
+                console.log("3");
                 CameraPreview.hide();
                 // Image Working
+                console.log("4");
                 image.src = imageURI;
+                console.log("5");
                 image.onload = function() {
+                    console.log("6");
                     var canvas = document.getElementById("canvasMan");
                     canvas.width = image.width;
                     canvas.height = image.height;
-                    ctx = canvas.getContext('2d')
-                    ctx.drawImage(image, 0, 0, image.width, image.height);
-                    percent_cover = processPhoto(canvas)
+                    console.log("7");
+                    ctx = canvas.getContext('2d');
+                    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+                    percent_cover = processPhoto(canvas);
                     canvas.style = image.style;
                     document.getElementById("main-text").innerHTML = percent_cover.toFixed(2) + "% Canopy Cover";
                     image.style.display = "none";
@@ -233,20 +224,24 @@ var app = {
                 var BLUE_CUTOFF = 200;
                 count_canopy = 0;
                 total_size = canvas.width * canvas.height;
+                console.log(total_size);
                 for (i = 0; i < canvas.width; i++) {
                     for (j = 0; j < canvas.height; j++) {
                         Data = ctx.getImageData(i, j, 1, 1).data;
-                        // var HSV_Data = rgb2hsv(Data[0], Data[1], Data[2]);
-                        // var Hue = HSV_Data.h
-                        // var Saturation = HSV_Data.s;
-                        // var Brightness = HSV_Data.v;
+                        // var HSV_Data = rgbToHsv(Data[0], Data[1], Data[2]);
+                        // var Hue = HSV_Data[0];
+                        // var Saturation = HSV_Data[1];
+                        // var Brightness = HSV_Data[2];
                         if ((Data[0] < RED_CUTOFF) || (Data[1] < GREEN_CUTOFF) || (Data[2] < BLUE_CUTOFF)) {
                             // console.log("Hue: " + HSV_Data.h + ". Saturation: " + HSV_Data.s + ". Brightness: " + HSV_Data.v);
                             // (Brightness <= 50) && 
-                            // if ((Hue < 170 || Hue > 250) && (Saturation > 13)) {
+                            // if ((Hue >= 170 && Hue =< 250) && (Saturation < 10)) {
+                                // ctx.fillStyle = "red";
+                                // ctx.fillRect(i, j, 1, 1);
+                                // ctx.stroke()
                             count_canopy += 1;
                         } else {
-                            ctx.fillStyle = "white";
+                            ctx.fillStyle = "red";
                             ctx.fillRect(i, j, 1, 1);
                             ctx.stroke();
                         }
@@ -255,46 +250,29 @@ var app = {
                 percent_cover = (count_canopy / total_size) * 100;
                 return (percent_cover);
             }
-
-            function rgb2hsv() { // From https://stackoverflow.com/questions/8022885/rgb-to-hsv-color-in-javascript?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-                var rr, gg, bb,
-                    r = arguments[0] / 255,
-                    g = arguments[1] / 255,
-                    b = arguments[2] / 255,
-                    h, s,
-                    v = Math.max(r, g, b),
-                    diff = v - Math.min(r, g, b),
-                    diffc = function(c) {
-                        return (v - c) / 6 / diff + 1 / 2;
-                    };
-
-                if (diff == 0) {
-                    h = s = 0;
+            function rgbToHsv(r, g, b) {  //From https://gist.github.com/mjackson/5311256
+                r /= 255, g /= 255, b /= 255;
+              
+                var max = Math.max(r, g, b), min = Math.min(r, g, b);
+                var h, s, v = max;
+              
+                var d = max - min;
+                s = max == 0 ? 0 : d / max;
+              
+                if (max == min) {
+                  h = 0; // achromatic
                 } else {
-                    s = diff / v;
-                    rr = diffc(r);
-                    gg = diffc(g);
-                    bb = diffc(b);
-
-                    if (r === v) {
-                        h = bb - gg;
-                    } else if (g === v) {
-                        h = (1 / 3) + rr - bb;
-                    } else if (b === v) {
-                        h = (2 / 3) + gg - rr;
-                    }
-                    if (h < 0) {
-                        h += 1;
-                    } else if (h > 1) {
-                        h -= 1;
-                    }
+                  switch (max) {
+                    case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                    case g: h = (b - r) / d + 2; break;
+                    case b: h = (r - g) / d + 4; break;
+                  }
+              
+                  h /= 6;
                 }
-                return {
-                    h: Math.round(h * 360),
-                    s: Math.round(s * 100),
-                    v: Math.round(v * 100)
-                };
-            }
+              
+                return [ h, s, v ];
+              }
         }
     },
 
